d5cff9241824c4958c295ea6e291402db9130255
Matthew Cox <mattdrummerguy@gmail.com>
2022-08-09 12:07:32 -0500

    updated url

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -58,47 +58,47 @@ class Navibar extends React.Component {
                         About Us
                       </Nav.Link>
                       <NavDropdown title="Site Content" id="basic-nav-dropdown">
-                        <NavDropdown.Item href="/view-products">
+                        <NavDropdown.Item href="./view-products">
                           View Products
                         </NavDropdown.Item>
-                        <NavDropdown.Item href="/reviews">
+                        <NavDropdown.Item href="./reviews">
                           Reviews
                         </NavDropdown.Item>
-                        <NavDropdown.Item href="/play-a-game" disabled>
+                        <NavDropdown.Item href="./play-a-game" disabled>
                           Play A Game
                         </NavDropdown.Item>
                       </NavDropdown>
                     </Nav>
                     <Button variant="outline-primary">
                       {" "}
-                      <Nav.Link href="/cart">View Cart</Nav.Link>
+                      <Nav.Link href="./cart">View Cart</Nav.Link>
                     </Button>
                   </Navbar.Collapse>
                 </Container>
               </Navbar>
               <br />
               <Routes>
-                <Route exact path="/homePage" element={<HomePage />}></Route>
+                <Route exact path="./homePage" element={<HomePage />}></Route>
                 <Route
                   exact
-                  path="/view-products"
+                  path="./view-products"
                   element={<ViewProducts />}
                 ></Route>
-                <Route exact path="/reviews" element={<Reviews />}></Route>
-                <Route exact path="/cart" element={<ViewCart />}></Route>
+                <Route exact path="./reviews" element={<Reviews />}></Route>
+                <Route exact path="./cart" element={<ViewCart />}></Route>
                 <Route
                   exact
-                  path="/"
-                  element={<Navigate to="/homePage" />}
+                  path="./"
+                  element={<Navigate to="./homePage" />}
                 ></Route>
                 <Route
                   exact
-                  path="/matts-final-project"
-                  element={<Navigate to="/homePage" />}
+                  path="./matts-final-project"
+                  element={<Navigate to="./homePage" />}
                 ></Route>
                 <Route
                   exact
-                  path="/kayak-1"
+                  path="./kayak-1"
                   element={<ProductDetails />}
                 ></Route>
               </Routes>
